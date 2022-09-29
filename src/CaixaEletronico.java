9b10ef8b9cc2827bb4fac3344e599951e554cc00
Hejociara <hejociara@gmail.com>
2022-09-29 19:09:23 -0300

    Feature

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
-        if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
+        if(valorSolicitado < saldo){
+            
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saldo atual: " + saldo);
+        }
+        else
+        System.out.println("Saldo insuficiente");
 
-        System.out.println(saldo);
     }
 }
